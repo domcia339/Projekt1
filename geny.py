159240dea207dfb92c81b9ebc5dec624b1cc8159
domcia339 <dominika.dziurkowska05@gmail.com>
2018-03-25 13:59:46 +0200

    Udało mi się zrobić generacje wszystkich możliwości chromosomów (810 000 - mam nadzieję, że to jest ok). Starałam sie w miarę wszystko opisywać, ale ogólnie to ten kod jest trochę słaby, trzeba dorobić jakieś klasy bo to średnio wygląda, ale jakoś nie mam pomysłu jak to dobrze zrobić.

--- geny.py
+++ geny.py
@@ -1,37 +1,49 @@
-import random
-##import xlwt #Excel library
+import itertools
 
-##total: do tylu się sumuje, n: na tyle liczb ma się składać
-def constrained_sum_sample_pos(n, total):
-    """Return a randomly chosen list of n positive integers summing to total.
-    Each such list is equally likely to occur."""
-    dividers = sorted(random.sample(range(0, total), n - 1))
-    return [a - b for a, b in zip(dividers + [total], [0] + dividers)]
+#total: do tylu się sumuje, n: na tyle liczb ma się składać
+def lista_mozliwych_genow( n, total):
+    allPossibleGene = [x for x in range(0, int(total)+1)]
+    tab=[x for x in itertools.product(allPossibleGene, repeat=n) if sum(x) == int(total)]
+    b = [list(x) for x in tab]
+    return b
 
-def constrained_sum_sample_nonneg(n, total):
-    """Return a randomly chosen list of n nonnegative integers summing to total.
-    Each such list is equally likely to occur."""
-    return [x - 1 for x in constrained_sum_sample_pos(n, total + n)]
+# Generuje listę możliwych genów dla wszystkich zapotrzebowań
+def generate_list_of_gens_for_all_demands( D, m_d, h_d):
+    gens=[]
+    for i in range(0, D):
+        lista = lista_mozliwych_genow(m_d[i], h_d[i])
+        gens.append(lista)
+    return gens
 
-##workbook = xlwt.Workbook(encoding="utf-8")
-##worksheet = workbook.add_sheet("generated-data")
+# Generuje tablice która ustala indeksy genow jakie będą wczytane do chromosomów
+def generate_possible_chromosoms(D, numberOfPossibleGens ):
 
-"""
-gens=[]
+    allPossibleQuanta = [x for x in range(0, int(max(numberOfPossibleGens)))]
+    #a = [x for x in itertools.product(allPossibleQuanta, repeat=6) if x[5] <= int(3) ]
+    chromosoms=[]
+    for x in itertools.product(allPossibleQuanta, repeat=D):
+        count = 0
+        for i in range(D):
+            if x[i] < numberOfPossibleGens[i]:
+                count=count+1
 
-for i in range (0, 8):
-    j = 0
-    gen=[]
-    #list = constrained_sum_sample_nonneg(15, 400)
-    list = constrained_sum_sample_pos(3, 8)
-    for n in list:
-        j = j+1
-        gen.append(n)
-        #worksheet.write(j, i, n)
-        ##print(n)
-    gens.append(gen)
+        if count == len(numberOfPossibleGens):
+            chromosoms.append(list(x))
+    return chromosoms
 
-print(gens)
-#workbook.save("FileName.csv")
+# Generuje liste wszyskich możliwych genów (tablic routingu)
+def generate_list_of_possible_chromosoms(D, numberOfPossibleGens, possibleGens):
+    possibleChromosoms= generate_possible_chromosoms(D, numberOfPossibleGens)
+    tableOfChromosom=[]
+    for i in possibleChromosoms:
+        a= []
+        for j in range(D):
+            a.append(possibleGens[j][i[j]])
 
-"""
+        tableOfChromosom.append(a)
+
+    theFile= open("tablice_routingu.txt", 'w')
+    for item in tableOfChromosom:
+        theFile.write("%s\n" % item)
+
+    return tableOfChromosom
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from random import randint
 from geny import*
 
+import itertools
+
 ## usunięcie putych linijek z pliku
-with open('dane.txt','r+') as file:
+with open('dane.txt', 'r+') as file:
     with open('dane2.txt', 'w+') as file2:
         for line in file:
             if not line.isspace():
@@ -13,52 +15,39 @@ f = open('dane2.txt', 'r')
 
 #Wczytuje liczbe krawedzi E
 E = int(f.readline())
-print(E)
+#print(E)
 
 links = []
-
 # Wczutuj tablice kwawędzi do tablicy dwuwymiarowej
 for i in range(E):
-    # e = f.readline().split()v
-    #ee = list(map(int, f.readline().split()))
     ee = [int(i) for i in f.readline().split()]
     links.append(ee)
-print(links)
 
 # Wczytuje liczbe zapotrzebowań
-D=int(f.readline())
-if(D<=0):
+D = int(f.readline())
+if D <= 0:
     D = int(f.readline())
-print(D)
 
-d=[0]*D
-p=[0]*D
-h_d=[0]*D
-m_d=[0]*D
+#Generuje puste tablice o odpowiedniej długości
+d = [0]*D
+p = [0]*D
+h_d = [0]*D
+m_d = [0]*D
 P={}  #Słownik
 # Generuje słownik sciezek
 for i in range(D):
-    #d[i], p[i], h_d[i] = list(map(int,f.readline().strip().split(' ')))
     d[i], p[i], h_d[i] = [int(a) for a in f.readline().strip().split(' ')]
-    m_d[i]=int(f.readline())
+    m_d[i] = int(f.readline())
     for j in range(m_d[i]):
-        #x=list(map(int,f.readline().strip().split(' ')))
         x = [int(a) for a in f.readline().strip().split(' ')]
-        P['P('+str(i+1)+','+str(j+1)+')']=x[1:]
-print(P)
+        P['P('+str(i+1)+','+str(j+1)+')'] = x[1:]
 
-#generacja chromosomów na podstawie funkcji z pliku geny.py
-gens=[]
-for i in range (0, 10):
-    j = 0
-    gen=[]
-    #list = constrained_sum_sample_nonneg(15, 400)
-    list = constrained_sum_sample_pos(m_d[0], h_d[0])
-    for n in list:
-        j = j+1
-        gen.append(n)
-        #worksheet.write(j, i, n)
-        ##print(n)
-    gens.append(gen)
+#Generacja możliwych genów na podstawie funkcji z pliku geny.py
+possibleGens = generate_list_of_gens_for_all_demands(D, m_d, h_d)
+print(possibleGens)
+numberOfPossibleGens=[]
+for i in range(D):
+    numberOfPossibleGens.append(len(possibleGens[i]))
 
-print(gens)
+#generacja możliwości ułożenia genów w chromosomie
+tableOfChromosoms= generate_list_of_possible_chromosoms(D, numberOfPossibleGens, possibleGens)
