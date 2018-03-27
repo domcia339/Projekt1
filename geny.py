3c0478ad5a53e8b7e05986c9b896faf1322db30b
domcia339 <dominika.dziurkowska05@gmail.com>
2018-03-28 00:00:08 +0200

    Dla małego zbioru już wszystko działa ok, wszystkie rozwiązania są optymalnie więc, nie trzeba krzyżować. Teraz trzeba zrobić mutacje dla dużego zbioru, nie mam jeszcze koncepcji po te wyniki są dosyć duże więc nie wiem czy to w ogóle da się zooptymalizować do 0, wątpie. Zmiany czy małe dane czy duże na początku pliku main.py.

--- geny.py
+++ geny.py
@@ -1,4 +1,7 @@
 import itertools
+import math
+import time
+import random
 
 #total: do tylu się sumuje, n: na tyle liczb ma się składać
 def lista_mozliwych_genow( n, total):
@@ -20,21 +23,45 @@ def generate_possible_chromosoms(D, numberOfPossibleGens ):
 
     allPossibleQuanta = [x for x in range(0, int(max(numberOfPossibleGens)))]
     #a = [x for x in itertools.product(allPossibleQuanta, repeat=6) if x[5] <= int(3) ]
+
     chromosoms=[]
     for x in itertools.product(allPossibleQuanta, repeat=D):
         count = 0
+
         for i in range(D):
             if x[i] < numberOfPossibleGens[i]:
                 count=count+1
 
         if count == len(numberOfPossibleGens):
             chromosoms.append(list(x))
+
+    return chromosoms
+
+
+# Generuje tablice która ustala indeksy genow jakie będą wczytane do chromosomów dla dużych zbiorów
+def generate_possible_chromosoms_BIG(D, numberOfPossibleGens ):
+
+    chromosoms=[]
+
+    for i in range(100):
+        random_chromosom = []
+        for j in range(D):
+            random_chromosom.append(random.randint(0, numberOfPossibleGens[j]-1))
+
+        chromosoms.append(random_chromosom)
+
     return chromosoms
 
 # Generuje liste wszyskich możliwych genów (tablic routingu)
 def generate_list_of_possible_chromosoms(D, numberOfPossibleGens, possibleGens):
-    possibleChromosoms= generate_possible_chromosoms(D, numberOfPossibleGens)
+
+    if D < 20:
+        possibleChromosoms= generate_possible_chromosoms(D, numberOfPossibleGens)
+    else:
+        possibleChromosoms = generate_possible_chromosoms_BIG(D, numberOfPossibleGens)
+
     tableOfChromosom=[]
+
     for i in possibleChromosoms:
         a= []
         for j in range(D):
@@ -47,3 +74,31 @@ def generate_list_of_possible_chromosoms(D, numberOfPossibleGens, possibleGens):
         theFile.write("%s\n" % item)
 
     return tableOfChromosom
+
+def minimize_function(E, tableOfChromosom,P, links ):
+
+    f_x = []
+    for chrom in tableOfChromosom:
+        link_loads = []
+
+        for item in range(1, E+1):
+            ll = 0
+            for i in P:
+                ii = P.index(i)
+
+                for j, x in enumerate(i):
+                    if item in x:
+                        #print(chromosom[ii][j])
+                        ll = ll + chrom[ii][j]
+
+            link_loads.append(ll)
+
+        y_e = []
+        for i in range(len(link_loads)):
+            y_e.append(math.floor(link_loads[i]/2))
+        #y_e_x = sum(y_e)
+        f_x.append( max( 0, sum(y_e) - links[0][2]*links[0][4]))
+
+    return f_x
+
+
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from random import randint
 from geny import*
+import math
 
 import itertools
 
 ## usunięcie putych linijek z pliku
-with open('dane.txt', 'r+') as file:
+with open('dane_duze.txt', 'r+') as file:
     with open('dane2.txt', 'w+') as file2:
         for line in file:
             if not line.isspace():
@@ -33,21 +34,42 @@ d = [0]*D
 p = [0]*D
 h_d = [0]*D
 m_d = [0]*D
-P={}  #Słownik
+P = []
 # Generuje słownik sciezek
 for i in range(D):
     d[i], p[i], h_d[i] = [int(a) for a in f.readline().strip().split(' ')]
     m_d[i] = int(f.readline())
+    a=[]
     for j in range(m_d[i]):
         x = [int(a) for a in f.readline().strip().split(' ')]
-        P['P('+str(i+1)+','+str(j+1)+')'] = x[1:]
+        ##P['P('+str(i+1)+','+str(j+1)+')'] = x[1:]
+        a.append(x[1:])
+    P.append(a)
+
 
 #Generacja możliwych genów na podstawie funkcji z pliku geny.py
 possibleGens = generate_list_of_gens_for_all_demands(D, m_d, h_d)
-print(possibleGens)
+
 numberOfPossibleGens=[]
 for i in range(D):
     numberOfPossibleGens.append(len(possibleGens[i]))
 
 #generacja możliwości ułożenia genów w chromosomie
+
 tableOfChromosoms= generate_list_of_possible_chromosoms(D, numberOfPossibleGens, possibleGens)
+
+#liczenie wartości funkcji f(x)
+minimize_f_x = minimize_function(E, tableOfChromosoms, P, links)
+print(minimize_f_x)
+
+#Mutacja
+
+#Wybieram 10 najmniejszych tablic
+tableOfChromosomsMutatioan = []
+for i in range(10):
+    min_chromosom = min(minimize_f_x)
+    tableOfChromosomsMutatioan.append(tableOfChromosoms[minimize_f_x.index(min_chromosom)])
+
+#for i in range(0,10, 2):
+
+
